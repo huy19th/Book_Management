810e4069f0806cabba2259e4515459eec1f9e7e7
nqhuy19 <112528789+nqhuy19@users.noreply.github.com>
2022-12-21 20:22:56 +0700

    split folder based on authorization

--- src/routers/register.router.ts
+++ src/routers/register.router.ts
@@ -1,21 +1,12 @@
 import express from 'express';
 import multer from 'multer';
 import registerController from '../controllers/register.controller';
-<<<<<<< HEAD
-import checkIfEmailExist from '../middlewares/checkIfEmalExists';
-=======
 import checkIfEmailExists from '../middlewares/checkIfEmailExists';
->>>>>>> 4297fc14139cd6d2c6a138122657c908441e7e90
 
 let upload = multer();
 let router = express.Router();
 
 router.get("/", registerController.showFormRegister);
-<<<<<<< HEAD
-router.post('/', upload.none(), checkIfEmailExist, registerController.register);
-
-=======
 router.post('/', upload.none(), checkIfEmailExists, registerController.register);
->>>>>>> 4297fc14139cd6d2c6a138122657c908441e7e90
 
 export default router;
